3b9e4a32cb4c874e990118dd842f7efef4edec39
GreyA <grey.altaer@gmail.com>
2025-03-17 16:49:40 -0700

    delete complete

--- src/App.jsx
+++ src/App.jsx
@@ -68,6 +68,22 @@ const App = () => {
     }
   };
 
+  const handleDeleteTrack = async (trackId) => {
+    try {
+      const deletedTrack = await trackService.deleteTrack(trackId);
+
+      if (deletedTrack.err) {
+        throw new Error(deletedTrack.err);
+      }
+
+      setTracks(tracks.filter((track) => track._id !== deletedTrack._id));
+      setSelected(null);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <TrackList
@@ -83,7 +99,11 @@ const App = () => {
           handleUpdateTrack={handleUpdateTrack}
         />
       ) : (
-        <TrackDetail selected={selected} handleFormView={handleFormView} />
+        <TrackDetail
+        selected={selected}
+        handleFormView={handleFormView}
+        handleDeleteTrack={handleDeleteTrack}
+        />
       )}
     </>
   );
--- src/services/trackService.js
+++ src/services/trackService.js
@@ -40,10 +40,20 @@ const update = async (formData, trackId) => {
     }
 };
 
-console.log(await index());
+const deleteTrack = async (trackId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${trackId}`, {
+            method: 'DELETE',
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 export {
     index,
     create,
-    update
+    update,
+    deleteTrack
 };
